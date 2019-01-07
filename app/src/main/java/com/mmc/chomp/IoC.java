6efafa2b2b4b1517369ae5fe4ed82f10effabb3b
mzck <mateusz.cieplak@gft.com>
2019-01-07 12:28:15 +0100

    Change IP

--- app/src/main/java/com/mmc/chomp/IoC.java
+++ app/src/main/java/com/mmc/chomp/IoC.java
@@ -1,7 +1,7 @@
 package com.mmc.chomp;
 
 public class IoC {
-    public static final String IP_ADDRESS = "ws://10.0.2.2:8080/chomp"; // 51.38.129.34 :: 10.0.2.2
+    public static final String IP_ADDRESS = "ws://51.38.129.34:8080/chomp"; // 51.38.129.34 :: 10.0.2.2
     private static Client client;
     private static String USER_ID = LoginKeeper.getInstance().getUserId();
 
